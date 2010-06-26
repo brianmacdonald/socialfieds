977a9fa868d2c98481e797e9917384c5df18f97e
Brian Macdonald <brian.m17@gmail.com>
2010-06-26 15:03:17 -0400

    More whitespace fixes.

--- socialfieds/admin.py
+++ socialfieds/admin.py
@@ -1,41 +1,41 @@
 from django.contrib import admin
-from models import *
+from models import Board, Category, Listing, ListingFlag
 from forms import CategoryAdminForm
 
 class BoardAdmin(admin.ModelAdmin):
-        list_display=('title',)
-        ordering = ['order']
-        list_per_page = 25
-        search_fields = ['title',]
-        prepopulated_fields = {'slug':('title',) }
+    list_display=('title',)
+    ordering = ['order']
+    list_per_page = 25
+    search_fields = ['title',]
+    prepopulated_fields = {'slug':('title',) }
 
 admin.site.register(Board, BoardAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
-        list_display=('title',)
-        ordering = ['order']
-        list_per_page = 25
-        search_fields = ['title',]
-        prepopulated_fields = {'slug':('title',) }
-        form = CategoryAdminForm       
+    list_display=('title',)
+    ordering = ['order']
+    list_per_page = 25
+    search_fields = ['title',]
+    prepopulated_fields = {'slug':('title',) }
+    form = CategoryAdminForm       
 
 admin.site.register(Category, CategoryAdmin)
 
 class ListingAdmin(admin.ModelAdmin):
-        list_display=('title', 'created')
-        ordering = ['-created']
-        list_per_page = 25
-        search_fields = ['title', 'description']
-        date_hierarchy = 'created'
-        prepopulated_fields = {'slug':('title',) }
+    list_display=('title', 'created')
+    ordering = ['-created']
+    list_per_page = 25
+    search_fields = ['title', 'description']
+    date_hierarchy = 'created'
+    prepopulated_fields = {'slug':('title',) }
 
 admin.site.register(Listing, ListingAdmin)
 
 class ListingFlagAdmin(admin.ModelAdmin):
-        list_display=('listing', 'flag', 'flag_date')
-        ordering = ['-flag_date']
-        list_per_page = 25
-        search_fields = ['listing', 'flag']
-        date_hierarchy = 'flag_date'
+    list_display=('listing', 'flag', 'flag_date')
+    ordering = ['-flag_date']
+    list_per_page = 25
+    search_fields = ['listing', 'flag']
+    date_hierarchy = 'flag_date'
 
 admin.site.register(ListingFlag, ListingFlagAdmin)
